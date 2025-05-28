91f49f48cb631214b4d0cc26531cf55ca112480d
AbrilGarciaBustos <20223tn053@utez.edu.mx>
2025-05-28 17:47:02 -0600

    [FIRST COMMIT SUBIDA INICIAL]

---
+++ src/main/java/utez/edu/mx/praexamen/PraexamenApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.praexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraexamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraexamenApplication.class, args);
+    }
+
+}
